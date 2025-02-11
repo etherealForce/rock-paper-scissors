65c96a91c2e25580804ddaf1536449096bca9472
Lu Win Maw <llaurence689@gmail.com>
2025-02-12 00:09:24 +0300

    Implement computer choice

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,16 @@ function getComputerChoice() {
     let rand_num = Math.floor(Math.random() * 3);
 
     switch (rand_num) {
-        case
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        default:
+            break;
     }
 }
