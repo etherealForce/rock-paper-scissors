f8444f7c860832c9faa6c1a1f8e678b9fae4e01c
Lu Win Maw <llaurence689@gmail.com>
2025-02-12 12:23:59 +0300

    Finish game concept

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,60 @@
 "use strict"
+let humanScore = 0, computerScore = 0;
+
+function playGame() {
+    
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+}
+
+if (humanScore > computerScore) {
+    console.log("Victory!");
+} else if (humanScore < computerScore ) {
+    console.log("Defeat.")
+} else {
+    console.log("A tie in the end.")
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is a tie.`;
+            } else if (computerChoice === "paper") {
+                ++computerScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            } else if (computerChoice === "scissors") {
+                ++humanScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            }
+            break;
+        case "paper":
+            if (computerChoice === "rock") {
+                ++humanScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            } else if (computerChoice === "paper") {
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is a tie.`;
+            } else if (computerChoice === "scissors") {
+                ++computerScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                ++computerScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            } else if (computerChoice === "paper") {
+                ++humanScore;
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            } else if (computerChoice === "scissors") {
+                return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is indeed a tie.`;
+            }
+            break;
+    }
+}
 
 
 function getComputerChoice() {
@@ -18,4 +74,17 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let answer = prompt("Choose your weapon.").toLowerCase();
+
+    while (true) {
+        if (answer !== "rock" 
+            && answer !== "paper" 
+            && answer !== "scissors") {
+            console.log("thats not a given choice, dillweed");
+        } else {
+            return answer;
+        }
+    }
+
+}
