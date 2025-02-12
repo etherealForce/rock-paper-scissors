b888e70cbef5a2e442504ee7d931749e9d93394d
Lu Win Maw <llaurence689@gmail.com>
2025-02-12 11:45:38 +0300

    Reimplement computerchoice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 "use strict"
+
+
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random * 3);
+    switch (randNum) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        default:
+            break;
+    }
+}
