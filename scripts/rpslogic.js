4179a86dee59d51519273b0300efc7230be95d15
Lu Win Maw <llaurence689@gmail.com>
2025-02-24 14:32:45 +0300

    Reimplement playRound

--- scripts/rpslogic.js
+++ scripts/rpslogic.js
@@ -1,60 +1,46 @@
 "use strict"
+let computerScore = 0, humanScore = 0;
+const report = document.querySelector(".report");
 
-// this function plays the whole game
-function playGame() {
-    let humanScore = 0, computerScore = 0;
 
-    // this function nested inside, plays 1 round of rps, returning each result of each round and updating scores
-    function playRound(humanChoice, computerChoice) {
-        switch (humanChoice) {
-            case "rock":
-                if (computerChoice === "rock") {
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is a tie.`;
-                } else if (computerChoice === "paper") {
-                    ++computerScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
-                } else if (computerChoice === "scissors") {
-                    ++humanScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
-                }
-                break;
-            case "paper":
-                if (computerChoice === "rock") {
-                    ++humanScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
-                } else if (computerChoice === "paper") {
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is a tie.`;
-                } else if (computerChoice === "scissors") {
-                    ++computerScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
-                }
-                break;
-            case "scissors":
-                if (computerChoice === "rock") {
-                    ++computerScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
-                } else if (computerChoice === "paper") {
-                    ++humanScore;
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
-                } else if (computerChoice === "scissors") {
-                    return `You picked ${humanChoice}\nComputer picked ${computerChoice} so it is indeed a tie.`;
-                }
-                break;
-        }
+// this function nested inside, plays 1 round of rps, returning each result of each round and updating scores
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        report.innerText = `Both of you chose ${humanChoice}, it was a tie!`;
     }
 
+    switch(humanChoice) {
+        case "rock":
+            if (computerChoice === "paper") {
+                ++computerScore;
+                report.innerText =  `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            } else if (computerChoice === "scissors") {
+                ++humanScore;
+                report.innerText = `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            }
+            break;
+        case "paper":
+            if (computerChoice === "rock") {
+                ++humanScore;
+                report.innerText = `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            } else if (computerChoice === "scissors") {
+                ++computerScore;
+                report.innerText = `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                ++computerScore;
+                report.innerText = `You picked ${humanChoice}\nComputer picked ${computerChoice} so you lose.`;
+            } else if (computerChoice === "paper") {
+                ++humanScore;
+                report.innerText = `You picked ${humanChoice}\nComputer picked ${computerChoice} so you win.`;
+            }
+            break; 
+    }}
 
 
-    // Determine the final result of the battle
-    if (humanScore > computerScore) {
-        console.log("Victory!");
-    } else if (humanScore < computerScore ) {
-        console.log("Defeat.")
-    } else {
-        console.log("A tie in the end.")
-    }
-    
-}
+
 
 // return computer's random choice of rock,paper or scissors
 function getComputerChoice() {
@@ -62,13 +48,10 @@ function getComputerChoice() {
     switch (randNum) {
         case 0:
             return "rock";
-            break;
         case 1:
             return "paper";
-            break;
         case 2:
             return "scissors";
-            break;
         default:
             break;
     }
@@ -95,8 +78,6 @@ function getHumanChoice() {
     }
 
 }
-
-
 
 const options = document.querySelector(".options.list");
 
